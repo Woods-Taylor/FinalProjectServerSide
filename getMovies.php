4e00613471d07b2b2c3e2bb85627333472372fd3
ericsson-james <jamesericsson95@gmail.com>
2019-10-08 17:34:19 -0400

    testing new feature

--- getMovies.php
+++ getMovies.php
@@ -2,5 +2,6 @@
 <html lang="en">
   <?php require 'functions.php';?>
   <?php noCache(); ?>
-  <?php testgetMovies($("#search")); ?>
+  <script type='text/javascript'>alert("PHP blows");</script>";
+  <!-- ?php testgetMovies($("#search")); ?> -->
 </html>
